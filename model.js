ab0ef6ca6d6b1a3291c041f0f005fba6f46cbf69
Saul Farber <saul@peoplegis.com>
2020-02-14 15:05:30 -0500

    whitespace cleanup, support param for geometry column and geometry srid add metadata to output

--- model.js
+++ model.js
@@ -12,8 +12,10 @@ const squel = require('squel').useFlavour('postgres');
 const pgp = require('pg-promise')();
 const dbgeo = require('dbgeo');
 
+let db;
 
-function Model (koop) {}
+
+function Model(koop) { }
 
 // Public function to return data from the
 // Return: GeoJSON FeatureCollection
@@ -27,53 +29,63 @@ function Model (koop) {}
 // req.params.method
 
 function formatSQL(req) {
-  const sql = squel.select()
-    .field(req.query.columns)
-    // .field(`ST_Simplify(ST_Transform(${'geom'}, 4326), 0.000001) as geom`)
-    .field(`ST_Transform(${'geom'}, 4326) as geom`)
-    .from(req.query.table)
-    // .where(req.query.filter)
-    // .limit(req.query.limit)
-    ;
-  
-  // if (req.query.join) {
-  //   const joint = req.query.join.split(';');
-  //   sql.join(join[0, null, join[1]]);
-  // }
-  // if (req.query.bounds) {
-  //   var bounds = req.query.bounds.split(',');
-  //   if (bounds.length === 4) {
-  //       sql.where(`${req.query.geom_column} && ST_Transform(ST_MakeEnvelope(${bounds.join(',')} , 4326), find_srid('', '${req.params.table}', '${req.query.geom_column}'))`);
-  //   } else if (bounds.length === 3) {
-  //       var smBounds = sm.bbox(bounds[1], bounds[2], bounds[0]);
-  //       sql.where(`${req.query.geom_column} && ST_Transform(ST_MakeEnvelope(${smBounds.join(',')} , 4326), find_srid('', '${req.params.table}', '${req.query.geom_column}'))`);
-  //   }
-  // }
+	const geomCol = req.query.geom;
+	const srcSrid = req.query.srcsrid;
+	const sql = squel.select()
+		.field(req.query.columns)
+		// .field(`ST_Simplify(ST_Transform(${'geom'}, 4326), 0.000001) as geom`)
+		.field(`st_transform(st_setsrid("${geomCol}", ${srcSrid}), 4326) as geom`)
+		.from(req.query.table)
+		// .where(req.query.filter)
+		// .limit(req.query.limit)
+		;
+	console.log('sql', sql.toString());
 
-  return sql.toString();
+	// if (req.query.join) {
+	//   const joint = req.query.join.split(';');
+	//   sql.join(join[0, null, join[1]]);
+	// }
+	// if (req.query.bounds) {
+	//   var bounds = req.query.bounds.split(',');
+	//   if (bounds.length === 4) {
+	//       sql.where(`${req.query.geom_column} && ST_Transform(ST_MakeEnvelope(${bounds.join(',')} , 4326), find_srid('', '${req.params.table}', '${req.query.geom_column}'))`);
+	//   } else if (bounds.length === 3) {
+	//       var smBounds = sm.bbox(bounds[1], bounds[2], bounds[0]);
+	//       sql.where(`${req.query.geom_column} && ST_Transform(ST_MakeEnvelope(${smBounds.join(',')} , 4326), find_srid('', '${req.params.table}', '${req.query.geom_column}'))`);
+	//   }
+	// }
+
+	return sql.toString();
 }
 
 Model.prototype.getData = function (req, callback) {
-  console.log('req:', req.query);
-  let geojson = '';
-  let db = pgp(config.db.postgis); //this is a specific config
-  db
-  .query(formatSQL(req))
-  .then(function(data) {
-    dbgeo.parse(data, {
-      outputFormat: 'geojson',
-      precision: 6
-    }, function (error, result) {
-      console.log(error, result);
-      callback(error, result);
-    })
-  })
-  .catch(function(err) {
-    callback({
-      'error': 'error running query',
-      'error_details': err
-    });
-  });
+	console.log('req:', req.query);
+	let geojson = '';
+	db = db || pgp(config.db.postgis); //this is a specific config
+	db
+		.query(formatSQL(req))
+		.then(function (data) {
+			dbgeo.parse(
+				data,
+				{
+					outputFormat: 'geojson',
+					precision: 6
+				},
+				function (error, result) {
+					console.log(error, result);
+					result.metadata = {
+						"idField" : "id"
+					};
+					callback(error, result);
+				}
+			)
+		})
+		.catch(function (err) {
+			callback({
+				'error': 'error running query',
+				'error_details': err
+			});
+		});
 }
 
 module.exports = Model
